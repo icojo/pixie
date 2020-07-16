83eb767e8ad6e4d15728fd44e738c396775934f8
Adrian Filip <realadrianfilip@gmail.com>
2020-07-16 22:49:40 +0300

    Refactor (#2)
    * Cleanup
    
    * Refactor
    
    * Cleanup
    
    * Refactor

--- src/main/scala/Application.scala
+++ src/main/scala/Application.scala
@@ -1,5 +1,5 @@
 import java.io.{ File, OutputStream }
-import java.net.{ InetSocketAddress, ServerSocket }
+import java.net.{ InetSocketAddress, ServerSocket, Socket }
 import java.nio.file.Files
 import java.util.UUID
 
@@ -10,8 +10,9 @@ import org.json4s.jackson.JsonMethods.parse
 import org.json4s.jackson.Serialization.write
 import uzhttp.Response
 import uzhttp.server.Server
+import zio.blocking._
 import zio.console._
-import zio.{ App, Task }
+import zio.{ App, RIO, Task, ZManaged }
 
 import scala.sys.process._
 import scala.util.Try
@@ -26,58 +27,61 @@ object Application extends App {
   private val httpPort = 8080
 
   def run(args: List[String]) =
-    logic.exitCode
+    createStreamingContext().use { ssc =>
+      for {
+        generatedRows    <- Task("./blackbox.sh".lineStream)
+        resultsDirectory <- effectBlocking(Files.createTempDirectory("results").toString)
+        _                <- putStrLn("Results dir=" + resultsDirectory)
+        _                <- processStream(ssc, resultsDirectory, Seconds(10), socketHost, socketPort).fork
+        _ <- createServerSocket(socketPort).use {
+              case (_, socketOutputStream) =>
+                for {
+                  _ <- effectBlocking {
+                        generatedRows.foreach { input =>
+                          socketOutputStream.write(input.getBytes)
+                          socketOutputStream.write(newLine)
+                        }
+                      }.fork
+                  _ <- Server
+                        .builder(new InetSocketAddress(httpHost, httpPort))
+                        .handleSome {
+                          case req if req.uri.getPath == "/" =>
+                            readWindows(resultsDirectory).map(resp => Response.plain(write(resp)(DefaultFormats))).orDie
+                        }
+                        .serve
+                        .useForever
+                        .orDie
+                } yield ()
 
-  private final def createServerSocket(port: Int): Task[OutputStream] = Task {
-    val acceptor = new ServerSocket(port)
-    val socket   = acceptor.accept()
-    socket.getOutputStream
-  }
+            }
+      } yield ()
+    }.exitCode
 
-  private final def logic =
-    for {
-      generatedRows      <- Task("./blackbox.sh".lineStream)
-      resultsDirectory   <- Task(Files.createTempDirectory("results").toString)
-      _                  <- putStrLn("Results dir=" + resultsDirectory)
-      _                  <- processStream(resultsDirectory, Seconds(10), socketHost, socketPort).fork
-      socketOutputStream <- createServerSocket(socketPort)
-      _ <- Task {
-            generatedRows.foreach { input =>
-              socketOutputStream.write(input.getBytes)
-              socketOutputStream.write(newLine)
-            }
-          }.fork
-      _ <- Server
-            .builder(new InetSocketAddress(httpHost, httpPort))
-            .handleSome {
-              case req if req.uri.getPath == "/" =>
-                readWindows(resultsDirectory).map(resp => Response.plain(write(resp)(DefaultFormats))).orDie
-            }
-            .serve
-            .useForever
-            .orDie
-    } yield ()
+  private final def createServerSocket(port: Int): ZManaged[Blocking, Throwable, (Socket, OutputStream)] =
+    ZManaged.make(effectBlocking {
+      val acceptor = new ServerSocket(port)
+      val socket   = acceptor.accept()
+      (socket, socket.getOutputStream)
+    }) { case (s, _) => Task(s.close()).orDie }
 
   final case class Window(
     timestamp: Long,
     counts: List[String]
   )
 
-  private final def readWindows(resultsDir: String): Task[List[Window]] = Task {
+  private final def readWindows(resultsDir: String): RIO[Blocking, List[Window]] = effectBlocking {
     new File(resultsDir)
       .listFiles()
       .filter(_.isDirectory)
       .map(windowResultsDir =>
         Window(
-          timestamp = extractTimestamp(windowResultsDir),
+          timestamp = windowResultsDir.getName.split("-")(1).toLong,
           counts = readPartitionFileCounts(windowResultsDir)
         )
       )
       .toList
       .sortBy(_.timestamp)
   }
-
-  private final def extractTimestamp(dir: File): Long = dir.getName.split("-")(1).toLong
 
   import collection.JavaConverters._
   private final def readPartitionFileCounts(dir: File): List[String] =
@@ -88,31 +92,38 @@ object Application extends App {
       .flatMap(value => Files.readAllLines(value.toPath).asScala)
       .toList
 
-  //{ "event_type": "foo", "data": "lorem", "timestamp": 1594898762 }
   final case class GeneratedRow(event_type: String, data: String, timestamp: Long)
 
-  private final def processStream(resultsDir: String, windowDuration: Duration, socketHost: String, socketPort: Int) =
-    Task {
-
+  private final def createStreamingContext() =
+    zio.Managed.make(Task {
       val conf = new SparkConf().setMaster("local[2]").setAppName("Pixie")
       val ssc  = new StreamingContext(conf, Seconds(1))
       ssc.checkpoint(Files.createTempDirectory("pixie_checkpoints_" + UUID.randomUUID().toString).toString)
       ssc
-        .socketTextStream(socketHost, socketPort)
-        .map { row =>
-          implicit val formats: DefaultFormats.type = DefaultFormats
-          val r                                     = Try(parse(row).extract[GeneratedRow]).getOrElse(null)
-          r
-        }
-        .filter(_ != null)
-        .window(windowDuration, windowDuration) //slide == window
-        .map(_.event_type)
-        .countByValue()
-        .saveAsTextFiles(resultsDir + "/result")
+    })(sc => Task(sc.stop(true, true)).orDie)
 
-      ssc.start()
-      ssc.awaitTermination()
+  private final def processStream(
+    ssc: StreamingContext,
+    resultsDir: String,
+    windowDuration: Duration,
+    socketHost: String,
+    socketPort: Int
+  ) = Task {
+    ssc
+      .socketTextStream(socketHost, socketPort)
+      .map { row =>
+        implicit val formats: DefaultFormats.type = DefaultFormats
+        val r                                     = Try(parse(row).extract[GeneratedRow]).getOrElse(null)
+        r
+      }
+      .filter(_ != null)
+      .window(windowDuration, windowDuration) //slide == window
+      .map(_.event_type)
+      .countByValue()
+      .saveAsTextFiles(resultsDir + "/result")
 
-    }
+    ssc.start()
+    ssc.awaitTermination()
+  }
 
 }
